cfec6c91124eca1cb7245cd16a2d4eacbcc79a15
Jakub Stawicki <jakub.stawicki15@gmail.com>
2024-11-30 16:40:50 +0100

    Pierwszy test

--- foo.hpp
+++ foo.hpp
@@ -8,5 +8,26 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     // Twoja implementacja tutaj
-    return {};
+    std::vector<char> output;
+    int i = 0;
+    std::list<Human>::reverse_iterator itt = people.rbegin();
+    
+    do
+    {
+        itt->birthday();
+        
+        if (itt->isMonster())
+        {
+            output[i] = 'n';
+        }
+        else
+        {
+            output[i] = 'y';
+        }
+
+        i++;
+        itt++;
+    }while(itt != people.rend());
+    
+    return output;
 }
